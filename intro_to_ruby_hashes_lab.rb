aec1e9a238beee150d9be2a0bb163079f719a3ec
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-06-11 04:46:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,6 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  key_passed = key
   new_hash = {
     key => value
   }
