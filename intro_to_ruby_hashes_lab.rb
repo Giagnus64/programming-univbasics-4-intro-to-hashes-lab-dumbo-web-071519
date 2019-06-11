fdca7dd70b07a22d04b23e59c9a76f6ca0ab5bf5
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-06-11 04:40:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,20 @@
 def new_hash
   # return an empty hash
+  return new_hash = {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  return new_hash = {
+    key1: "value"
+  }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  return pioneer_hash = {
+    name: 'Grace Hopper'
+  }
 end
 
 def id_generator
